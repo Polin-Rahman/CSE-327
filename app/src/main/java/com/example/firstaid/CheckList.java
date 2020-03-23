585739e6324ec551711d805fe65f8c25d00845e6
Polin Rahman <61315426+Polin-Rahman@users.noreply.github.com>
2020-03-23 16:18:58 +0600

    Check list
    Add & retrive multiple data using Firestore

--- app/src/main/java/com/example/firstaid/CheckList.java
+++ app/src/main/java/com/example/firstaid/CheckList.java
@@ -1,14 +1,116 @@
 package com.example.firstaid;
 
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class CheckList extends AppCompatActivity {
+
+    private static final String TAG = "CheckList";
+
+    private static final String KEY_TITLE = "medicineName";
+    private static final String KEY_DESCRIPTION = "description";
+
+    private EditText editTextTitle;
+    private EditText editTextDescription;
+    private TextView textViewData;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference notebookRef = db.collection("Notebook");
+    private DocumentReference noteRef = db.document("Notebook/My First Note");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_check_list);
+
+        editTextTitle = findViewById(R.id.edit_text_title);
+        editTextDescription = findViewById(R.id.edit_text_description);
+        textViewData = findViewById(R.id.text_view_data);
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        notebookRef.addSnapshotListener(this, new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(QuerySnapshot queryDocumentSnapshots, FirebaseFirestoreException e) {
+                if (e != null) {
+                    return;
+                }
+
+                String data = "";
+
+                for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                    Note note = documentSnapshot.toObject(Note.class);
+                    note.setDocumentId(documentSnapshot.getId());
+
+                    String documentId = note.getDocumentId();
+                    String title = note.getTitle();
+                    String description = note.getDescription();
+
+                    data += "ID: " + documentId
+                            + "\nMedicine Name: " + title + "\nDescription: " + description + "\n\n";
+                }
+
+                textViewData.setText(data);
+            }
+        });
+    }
+
+    public void saveNote(View v) {
+        String title = editTextTitle.getText().toString();
+        String description = editTextDescription.getText().toString();
+
+        Note note = new Note(title, description);
+
+        notebookRef.add(note);
+    }
+
+
+    public void loadNote(View v) {
+        notebookRef.get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        String data = "";
+
+                        for (QueryDocumentSnapshot documentSnapshot : queryDocumentSnapshots) {
+                            Note note = documentSnapshot.toObject(Note.class);
+                            note.setDocumentId(documentSnapshot.getId());
+
+                            String documentId = note.getDocumentId();
+                            String title = note.getTitle();
+                            String description = note.getDescription();
+
+                            data += "ID: " + documentId
+                                    + "\nMedicine Name: " + title + "\nDescription: " + description + "\n\n";
+                        }
+
+                        textViewData.setText(data);
+                    }
+                });
+    }
+
+
 }
---
+++ app/src/main/java/com/example/firstaid/Note.java
@@ -0,0 +1,36 @@
+package com.example.firstaid;
+
+import com.google.firebase.firestore.Exclude;
+
+public class Note {
+
+    private String documentId;
+    private String title;
+    private String description;
+
+    public Note() {
+        //public no-arg constructor needed
+    }
+
+    @Exclude
+    public String getDocumentId() {
+        return documentId;
+    }
+
+    public void setDocumentId(String documentId) {
+        this.documentId = documentId;
+    }
+
+    public Note(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
